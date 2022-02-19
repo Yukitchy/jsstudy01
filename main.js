25447edf57e79aca363f067a540d94663c582f1f
YuukiAnimeSensei <icchan417@gmail.com>
2022-02-19 12:57:35 +0900

    Supabaseのライブラリ利用例を追加

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+
+const main = async () => {
+    const supabaseUrl = 'https://jebiwvlssrtnvtxlgncm.supabase.co'
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzUyMjg4OCwiZXhwIjoxOTU5MDk4ODg4fQ.28Be0hzH5THtt7Jjd0jY45Hrt741D68T2jZke6O0Mvc'
+    const client = supabase.createClient(supabaseUrl, supabaseKey)
+    console.log(client);
+    //SELECT prefecture, city from whereiam ORDER BY created_at desc LIMIT 1;  
+    let { data: whereiam, error } = await client
+        .from('whereiam')
+        .select('*')
+        .order('created_at', { ascending: false })
+        .limit(1)
+    console.log(whereiam);
+}
+
+main();
